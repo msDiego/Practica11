11228432d50404735598c9465df56109f213abb2
msDiego <dmorrosilva@cifpfbmoll.eu>
2023-03-01 02:25:03 +0100

    Añadida lógica de negocio.

---
+++ src/main/java/com/daw/practica11/configuration/RoutesConfig.java
@@ -0,0 +1,10 @@
+package com.daw.practica11.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class RoutesConfig implements WebMvcConfigurer {
+
+
+}
--- src/test/java/com/daw/practica11/Practica11ApplicationTests.java
+++ src/test/java/com/daw/practica11/Practica11ApplicationTests.java
@@ -7,7 +7,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Practica11ApplicationTests {
 
     @Test
-    void contextLoads() {
+     void testCalculadora(){
+
+        int totalValue = 200000;
+        int numCuotas = 60;
+        double interesAnual = 0.03;
+        double cAmortizable = (totalValue * (interesAnual / 12) ) / (1 - Math.pow(1+interesAnual/12, -numCuotas));
+        double totalPagado = 0;
+        double interes;
+        double amortizado;
+
+        while (totalPagado < totalValue * (1 + interesAnual)){
+
+            amortizado = cAmortizable - ( (totalValue - totalPagado) * interesAnual / 12);
+            interes = cAmortizable - amortizado;
+            totalPagado = totalPagado + cAmortizable;
+
+            System.out.println("Total pagado: " + totalPagado + " | Interés: " + interes + " | Amortizado: " + amortizado );
+
+        }
+
+
     }
 
 }
